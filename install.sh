9e295efb0b7c91f97df66132fe94ed8f4f299bc1
Lydian Lee <tingyenl@yelp.com>
2014-08-31 16:31:07 -0700

    Include my vim configs

--- install.sh
+++ install.sh
@@ -8,3 +8,8 @@ ln -fs ~/.dotfiles/tmux.conf .tmux.conf
 ln -fs ~/.dotfiles/git-completion.bash .git-completion.bash
 ln -fs ~/.dotfiles/remotecopyrc .remotecopyrc
 ln -fs ~/.dotfiles/ssh-config .ssh/config
+
+# Vim
+ln -fs ~/.dotfiles/vimrc/vimrc .vimrc
+~/.dotfiles/vimrc/install.sh
+
